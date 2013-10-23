5fa00116633669bda75b7463d39df11275006001
Clare Glinka <glinka.cb@gmail.com>
2013-10-23 11:33:35 -0500

    Add posts helper method to populate sidebar with links to blog posts.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  # Generate a link to a post for the side nav.
+  #
+  # post         - the Post to link to.
+  # current_post - the Post currently being viewed.
+  #
+  # Returns the markup for a line item in the side nav.
+  def post_link(post, current_post)
+    content_tag :li, class: ('active' if post == current_post) do
+      link_to post.title, post
+    end
+  end
 end
